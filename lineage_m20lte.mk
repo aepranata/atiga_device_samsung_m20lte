d369fd1d67eeab50cfacfc00d944b6f000227686
Aditya Eka Pranata <aepranata@gmail.com>
2025-08-28 15:59:12 +0700

    m20lte: Add `LINEAGE_MAINTAINER` flag

--- lineage_m20lte.mk
+++ lineage_m20lte.mk
@@ -2,5 +2,6 @@ $(call inherit-product, device/samsung/m20lte/full_m20lte.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+LINEAGE_MAINTAINER := aepranata
 
 PRODUCT_NAME := lineage_m20lte
